96004e85091b54089a2fb1589835d6882cee3d03
minhnd18-02 <lammjnphong4560@gmail.com>
2024-03-15 11:26:16 +0700

    other task

--- app/src/main/java/com/example/homework2/MainActivity.kt
+++ app/src/main/java/com/example/homework2/MainActivity.kt
@@ -1,34 +1,45 @@
 package com.example.homework2
 
-import android.os.Bundle
-import android.widget.Button
-import android.widget.TextView
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.TextView
+import androidx.core.content.ContextCompat
 
+class MainActivity : AppCompatActivity() {
 
-class MainActivity: AppCompatActivity() {
-
-    private lateinit var message: TextView
-    private lateinit var clickHere: Button
-
-    private var isTextChanged = false
+    private var counter: Int = 10
+    private lateinit var counterTextView: TextView
+    private lateinit var rootView: View
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        counterTextView = findViewById(R.id.counterTextView)
+        rootView = findViewById(android.R.id.background)
 
-        message = findViewById(R.id.startText)
-        clickHere = findViewById(R.id.btnChangeText)
+        updateCounterText()
+    }
 
-        clickHere.setOnClickListener {
-            if (isTextChanged) {
-                message.text = getString(R.string.welcome_text)
-            } else {
-                message.text = getString(R.string.text_changed_message)
-            }
-            isTextChanged = !isTextChanged
+    fun onClick(view: View) {
+        if (counter > 0) {
+            counter--
+            updateCounterText()
         }
+        if (counter == 0) {
+            rootView.setBackgroundColor(ContextCompat.getColor(this, R.color.red))
+        }
+    }
 
+    fun onResetClick(view: View) {
+        counter = 10
+        updateCounterText()
+        rootView.setBackgroundColor(ContextCompat.getColor(this, R.color.appBackground))
+    }
+
+    private fun updateCounterText() {
+        counterTextView.text = counter.toString()
     }
 }
